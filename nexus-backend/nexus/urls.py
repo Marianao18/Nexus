eeaf57bc67174d77d3795f9c78258d1e0d983567
Marianao18 <ospinam265@gmail.com>
2026-06-05 16:52:53 -0500

    arreglo bug foto docente V.2

--- nexus-backend/nexus/urls.py
+++ nexus-backend/nexus/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
-#urlpatterns = [
-   # path('admin/', admin.site.urls),
-  #  path('api/usuarios/',   include('apps.usuarios.urls')),
-   # path('api/solicitudes/',   include('apps.solicitudes.urls')),
-   # path('api/cursos/',   include('apps.cursos.urls')),
-    
-#] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,4 +11,7 @@ urlpatterns = [
     path('api/', include('apps.usuarios.urls')),
     path('api/', include('apps.solicitudes.urls')),
     path('api/', include('apps.cursos.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # Servir archivos /media/ siempre (dev y prod).
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
